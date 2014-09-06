8a958de15c9d3e389965c9848d74a5943d3a2d50
Markus Barenhoff <alios@alios.org>
2014-09-06 21:59:16 +0200

    added tests for hexagon diagonals,neighbors and distances

--- tests/HexagonTests.hs
+++ tests/HexagonTests.hs
@@ -17,8 +17,8 @@ instance Arbitrary Offset where
 instance Arbitrary OffsetCoordinate where
   arbitrary = do
     (offset, orientation) <- arbitrary
-    q <- arbitrary `suchThat` (>= 0)
-    r <- arbitrary `suchThat` (>= 0)
+    q <- arbitrary -- `suchThat` (>= 0)
+    r <- arbitrary -- `suchThat` (>= 0)
     return $
       OffsetCoordinate { offset_offset = offset
                        , offset_orientation = orientation
@@ -90,12 +90,202 @@ conversionSpec =
     it "convert from axial to offset and back" $ property prop_a2o
     it "convert from offset to axial and back" $ property prop_o2a
 
+prop_neighbors_cube_axial :: CubeCoordinate -> Bool
+prop_neighbors_cube_axial c =
+  let cs = cubeNeighbors c
+      a = cube2axial c
+      as = axialNeighbors a
+      cas = fmap axial2cube as
+  in cas `eqList` cs
+prop_neighbors_cube_offset :: Offset -> CubeCoordinate -> Bool
+prop_neighbors_cube_offset o c =
+  let cs = cubeNeighbors c
+      a = cube2offset o c
+      as = offsetNeighbors a
+      cas = fmap offset2cube as
+  in cas `eqList` cs
+
+prop_neighbors_axial_cube :: AxialCoordinate -> Bool
+prop_neighbors_axial_cube c =
+  let cs = axialNeighbors c
+      a = axial2cube c
+      as = cubeNeighbors a
+      cas = fmap cube2axial as
+  in cas `eqList` cs
+
+prop_neighbors_axial_offset :: Offset -> AxialCoordinate -> Bool
+prop_neighbors_axial_offset o c =
+  let cs = axialNeighbors c
+      a = axial2offset o c
+      as = offsetNeighbors a
+      cas = fmap offset2axial as
+  in cas `eqList` cs
+
+prop_neighbors_offset_axial :: OffsetCoordinate -> Bool
+prop_neighbors_offset_axial c =
+  let cs = offsetNeighbors c
+      a = offset2axial c
+      as = axialNeighbors a
+      o = offset_offset c
+      cas = fmap (axial2offset o) as
+  in cas `eqList` cs
+
+prop_neighbors_offset_cube :: OffsetCoordinate -> Bool
+prop_neighbors_offset_cube c =
+  let cs = offsetNeighbors c
+      a = offset2cube c
+      as = cubeNeighbors a
+      o = offset_offset c
+      cas = fmap (cube2offset o) as
+  in cas `eqList` cs
+
+
+neighborsSpec :: Spec
+neighborsSpec =
+  describe "testing distance by" $ do
+    it "comapring cubeNeighbors to axialNeighbors from cube" $
+       property prop_neighbors_cube_axial
+    it "comapring cubeNeighbors to offsetNeighbors from cube" $
+       property prop_neighbors_cube_offset
+    it "comapring axialNeighbors to cubeNeighbors from axial" $
+       property prop_neighbors_axial_cube
+    it "comapring axialNeighbors to offsetNeighbors from axial" $
+       property prop_neighbors_axial_offset
+    it "comapring offsetNeighbors to axialNeighbors from offset" $
+       property prop_neighbors_offset_axial
+    it "comapring offsetNeighbors to cubeNeighbors from offset" $
+       property prop_neighbors_offset_cube
+
+
+
+prop_diagonals_cube_axial :: CubeCoordinate -> Bool
+prop_diagonals_cube_axial c =
+  let cs = cubeDiagonals c
+      a = cube2axial c
+      as = axialDiagonals a
+      cas = fmap axial2cube as
+  in cas `eqList` cs
+prop_diagonals_cube_offset :: Offset -> CubeCoordinate -> Bool
+prop_diagonals_cube_offset o c =
+  let cs = cubeDiagonals c
+      a = cube2offset o c
+      as = offsetDiagonals a
+      cas = fmap offset2cube as
+  in cas `eqList` cs
+
+prop_diagonals_axial_cube :: AxialCoordinate -> Bool
+prop_diagonals_axial_cube c =
+  let cs = axialDiagonals c
+      a = axial2cube c
+      as = cubeDiagonals a
+      cas = fmap cube2axial as
+  in cas `eqList` cs
+
+prop_diagonals_axial_offset :: Offset -> AxialCoordinate -> Bool
+prop_diagonals_axial_offset o c =
+  let cs = axialDiagonals c
+      a = axial2offset o c
+      as = offsetDiagonals a
+      cas = fmap offset2axial as
+  in cas `eqList` cs
+
+prop_diagonals_offset_axial :: OffsetCoordinate -> Bool
+prop_diagonals_offset_axial c =
+  let cs = offsetDiagonals c
+      a = offset2axial c
+      as = axialDiagonals a
+      o = offset_offset c
+      cas = fmap (axial2offset o) as
+  in cas `eqList` cs
+
+prop_diagonals_offset_cube :: OffsetCoordinate -> Bool
+prop_diagonals_offset_cube c =
+  let cs = offsetDiagonals c
+      a = offset2cube c
+      as = cubeDiagonals a
+      o = offset_offset c
+      cas = fmap (cube2offset o) as
+  in cas `eqList` cs
+
+
+diagonalsSpec :: Spec
+diagonalsSpec =
+  describe "testing diagonals by" $ do
+    it "comapring cubeDiagonals to axialDiagonals from cube" $
+       property prop_diagonals_cube_axial
+    it "comapring cubeDiagonals to offsetDiagonals from cube" $
+       property prop_diagonals_cube_offset
+    it "comapring axialDiagonals to cubeDiagonals from axial" $
+       property prop_diagonals_axial_cube
+    it "comapring axialDiagonals to offsetDiagonals from axial" $
+       property prop_diagonals_axial_offset
+    it "comapring offsetDiagonals to axialDiagonals from offset" $
+       property prop_diagonals_offset_axial
+    it "comapring offsetDiagonals to cubeDiagonals from offset" $
+       property prop_diagonals_offset_cube
+
+prop_distance_axial_cube :: AxialCoordinate -> AxialCoordinate -> Bool
+prop_distance_axial_cube a b =
+  let cd = distance (axial2cube a) (axial2cube b)
+  in cd == distance a b
+
+prop_distance_axial_offset :: Offset -> AxialCoordinate -> AxialCoordinate -> Bool
+prop_distance_axial_offset o a b =
+  let cd = distance (axial2offset o a) (axial2offset o b)
+  in cd == distance a b
+
+prop_distance_cube_axial :: CubeCoordinate -> CubeCoordinate -> Bool
+prop_distance_cube_axial a b =
+  let cd = distance (cube2axial a) (cube2axial b)
+  in cd == distance a b
+prop_distance_cube_offset :: Offset -> CubeCoordinate -> CubeCoordinate -> Bool
+prop_distance_cube_offset o a b =
+  let cd = distance (cube2offset o a) (cube2offset o b)
+  in cd == distance a b
+
+prop_distance_offset_axial :: OffsetCoordinate -> OffsetCoordinate -> Bool
+prop_distance_offset_axial a b =
+  let cd = distance (offset2axial a) (offset2axial b)
+  in cd == distance a b
+
+prop_distance_offset_cube :: OffsetCoordinate -> OffsetCoordinate -> Bool
+prop_distance_offset_cube a b =
+  let cd = distance (offset2cube a) (offset2cube b)
+  in cd == distance a b
+
+
+
+distanceSpec :: Spec
+distanceSpec =
+  describe "testing neighbors by" $ do
+    it "comapring cubeDistance to axialDistance from cube" $
+       property prop_distance_cube_axial
+    it "comapring cubeDistance to offsetDistance from cube" $
+       property prop_distance_cube_offset
+    it "comapring axialDistance to cubeDistance from axial" $
+       property prop_distance_axial_cube
+    it "comapring axialDistance to offsetDistance from axial" $
+       property prop_distance_axial_offset
+    it "comapring offsetDistance to axialDistance from offset" $
+       property prop_distance_offset_axial
+    it "comapring offsetDistance to cubeDistance from offset" $
+       property prop_distance_offset_cube
+
+
+
 hexagonSpec :: Spec
 hexagonSpec =
   describe "testing Hexagon" $ do
     conversionSpec
+    neighborsSpec
+    distanceSpec
+    diagonalsSpec
+
+main = hspec hexagonSpec
+main :: IO ()
+
+eqList :: Eq a => [a] -> [a] -> Bool
+eqList cs ds = (length cs == length ds) && (and [elem c ds | c <- cs ])
 
 
-runHexagonTests = hspec hexagonSpec
-runHexagonTests :: IO ()
 
